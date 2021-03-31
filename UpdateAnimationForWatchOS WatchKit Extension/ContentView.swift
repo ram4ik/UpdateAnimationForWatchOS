ac10f7d0e15b0d8eba6154ff444afb6043433ade
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-31 20:45:24 +0300

    init

--- UpdateAnimationForWatchOS WatchKit Extension/ContentView.swift
+++ UpdateAnimationForWatchOS WatchKit Extension/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack(spacing: 10) {
+            ZStack {
+                progressView(progress: $progress)
+                    .padding()
+                Image(systemName: "swift")
+                    .font(.system(size: 80))
+                    .foregroundColor(.blue)
+            }
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .onAppear() {
+            _ = Timer.scheduledTimer(withTimeInterval: 0.4, repeats: true, block: { (timer) in
+                withAnimation {
+                    progress = progress + 0.1
+                    if progress >  1 {
+                        progress = 0
+                    }
+                }
+            })
+        }
+    }
+}
+
+struct progressView: View {
+    @Binding var progress: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(style: StrokeStyle(lineWidth: 4, dash: [1, 2]))
+                .opacity(0.3)
+                .foregroundColor(.blue)
+            Circle()
+                .trim(from: 0.0, to: CGFloat(min(progress, 1.0)))
+                .stroke(style: StrokeStyle(lineWidth: 4, dash: [1, 2]))
+                .foregroundColor(.green)
+                .rotationEffect(Angle(degrees: 270))
+        }
     }
 }
 
